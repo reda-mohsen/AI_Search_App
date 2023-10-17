c3ad854d6af92b0dda694de0dfa022a493d409ee
Reda <ai.reda.mohsen@gmail.com>
2023-10-17 14:23:47 +0300

    Handle Exceptions and update BFS search algorithm

--- project.py
+++ project.py
@@ -1,76 +1,119 @@
+# Name: Reda Mohsen Reda
+# Project Title: AI Search Algorithms Application
 import networkx as nx
 import matplotlib.pyplot as plt
 import re
 import sys
 
-def main():
-    input_graph_edges = get_input_graph()
-    graph = get_graph(input_graph_edges)
-    if bfs(graph,"s","g") is not None:
-        print("Path:", *bfs(graph,"s","g"))
-    else:
-        print("Failed to reach path")
 
-def bfs(graph, start_node, goal_node):
-    if graph.__len__() < 1:
-        raise ValueError("Empty graph")
-    if not graph.has_node(start_node):
-        raise ValueError("Start node is not in graph")
-    if not graph.has_node(goal_node):
-        raise ValueError("Goal node is not in graph")
-    # create a queue
-    fringe = []
-    # create a list for visited nodes
-    visited = []
-    # create color map for nodes
-    color_map = []
-    visited.append(start_node)
-    fringe.append(start_node)
-    color_map.append('red')
-    while fringe:
-        start_node = fringe.pop(0)
-        for node in graph.successors(start_node):
-            color_map.append('blue')
-            if node not in visited:
-                fringe.append(node)
-                visited.append(node)
-            if node == goal_node:
-                color_map[-1]='green'
-                pos = nx.spring_layout(graph, seed=47)  # Seed layout for reproducibility
-                nx.draw(graph,pos=pos,node_color=color_map,edge_color='grey', with_labels = True)
-                for node in visited:
-                    title += node + " "
-                plt.suptitle(title.split(), fontweight ="bold")
-                plt.savefig("graph.png")
-                return visited
-    return
+def main():
+    graph = get_graph(get_input_graph())
+    cost_to_goal, path_to_goal = bfs(graph, "s", ["i", "g"])
+    if path_to_goal is not None:
+        print("Path:", *path_to_goal)
+        print("Cost:", cost_to_goal)
+    else:
+        print("Failed to reach goal nodes")
+
 
 def get_input_graph():
-    try:
-        graph_edges = []
-        while True:
-            print("Press ctrl+d to stop")
-            input_graph = input("Edge (as Node, Node): ")
-            if match := re.search(r"^(\w+)\, ?(\w+)", input_graph, re.IGNORECASE):
-                graph_edges.append((match.group(1), match.group(2)))
-            else:
-                print("Invalid Format")
-                continue
+    """
+     Prompt the user for input of graph edges with weights, validate the input, and return a list of graph edges.
 
-    except EOFError:
-        print()
-        return graph_edges
+     This function ensures that the user-provided graph edges are correctly formatted as (node, node=weight),
+     where 'node' can be any alphanumeric character, and 'weight' must be a non-negative integer. The input format
+     should be 'edge1+edge2+edge3+...' where each 'edge' conforms to the format mentioned.
+
+     Returns: list of tuples: A list of graph edges represented as tuples, where each tuple consists of two nodes and
+     a dictionary with the 'weight' attribute.
+
+     Example:
+     >>> get_input_graph()
+     Enter edges with weights as (node,node=weight+node,node=weight): A, B=3+C, D=2
+     [('A', 'B', {'weight': '3'}), ('C', 'D', {'weight': '2'})]
+
+     """
+    # infinite loop to make sure the input edges are correct
+    while True:
+        # get graph edges concatenated with '+' from the user
+        input_graph_edges = input("Enter edges with weights as (node,node=weight+node,node=weight): ").strip()
+        # get a list of graph edges
+        input_graph_edges = input_graph_edges.split("+")
+        # create a list to store graph edges in a correct format
+        graph_edges = []
+        for edge in input_graph_edges:
+            # check if the edge is in a correct format and append it the the list created
+            if match := re.search(r"^(\w+)\s*,\s*(\w+)\s*=\s*(\d+)$", edge.strip(), re.IGNORECASE):
+                graph_edges.append((match.group(1), match.group(2), {"weight": match.group(3)}))
+            # if user enter invalid input, return to input graph edges again
+            else:
+                print("Invalid edges")
+                break
+        # return list of graph edges
+        if len(graph_edges) == len(input_graph_edges):
+            return graph_edges
+
 
 def get_graph(graph_edges: list):
     try:
-        if len(graph_edges) != 0:
+        if graph_edges is not None:
             graph = nx.DiGraph()
-            graph.add_edges_from(graph_edges, color="red")
+            graph.add_edges_from(graph_edges)
             return graph
         else:
+            raise ValueError("Wrong graph edges")
+    except ValueError:
+        pass
+
+
+def bfs(graph: nx.DiGraph, start_node: str, goal_nodes: list):
+    try:
+        # check if graph is empty
+        if graph is None:
             raise ValueError("Empty graph")
+        # check if start node is not in graph
+        if not graph.has_node(start_node):
+            raise ValueError("Start node is not in graph")
+        # check if a goal node is not in graph
+        for node in goal_nodes:
+            if not graph.has_node(node):
+                raise ValueError(f"Goal node {node} is not in graph")
+        # store the current node, cost, and path in a queue
+        fringe = [(start_node, 0, [start_node])]
+        # create a list of visited nodes in graph
+        visited = set()
+        while fringe:
+            node, cost, path = fringe.pop(0)
+            if node in goal_nodes:
+                draw_graph(graph, start_node, goal_nodes, path, cost)
+                return cost, path
+            visited.add(node)
+            for neighbor in graph.neighbors(node):
+                if neighbor not in visited:
+                    new_cost = cost + int(graph[node][neighbor]['weight'])
+                    new_path = path + [neighbor]
+                    fringe.append((neighbor, new_cost, new_path))
+        return None, None
     except ValueError as err:
         sys.exit(err)
+
+
+def draw_graph(graph, start_node: str, goal_nodes: list, path: list, cost: int):
+    pos = nx.spring_layout(graph, seed=43)  # Seed layout for -  gh reproducibility
+    node_colors = ['red' if node == start_node
+                   else 'green' if node in goal_nodes
+                   else 'lightblue' for node in graph.nodes]
+    edge_colors = ['green' if (u, v) in zip(path, path[1:]) else 'black' for u, v in graph.edges]
+    nx.draw(graph, pos=pos, node_color=node_colors, node_size=500, edge_color=edge_colors, with_labels=True)
+    labels = nx.get_edge_attributes(graph, 'weight')
+    nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
+    title = "Path using BFS is: "
+    for path_node in path:
+        title += path_node + " "
+    title += "// Cost using BFS is: " + str(cost)
+    plt.suptitle(title.strip(), fontweight="bold")
+    plt.savefig("graph.png")
+
 
 if __name__ == "__main__":
     main()
